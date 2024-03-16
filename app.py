961b6224dda413347ff041c3c521772c59c735ec
Sabuat Urbina Ribeiro <sabuaturbina@yahoo.com>
2024-03-16 15:23:32 -0300

    Changes to be committed: 	modified:   app.py 	new file:   static/foto_sobre_2.jpg 	modified:   static/style.css 	modified:   templates/about.html 	modified:   templates/contact.html 	modified:   templates/index.html 	modified:   templates/store.html

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/store')
+def store():
+    return render_template('store.html')
 
 app.run(debug=True)
